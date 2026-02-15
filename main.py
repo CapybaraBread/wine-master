2718bfce3f161e8021cccdb9aba0c9dfc5011f9a
CapybaraBread <denis05102012@gmail.com>
2026-02-15 10:45:51 +0000

    Использовал файл Розы

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+import datetime
+from jinja2 import Environment, FileSystemLoader, select_autoescape # pyright: ignore[reportMissingImports]
+import pandas as pd
+from pprint import pprint
+exel_file = pd.read_excel('wine2.xlsx')
+data = exel_file.to_dict(orient='records')
+pprint(data)
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html'])
+)
+template = env.get_template('template.html')
+age = "{}".format(datetime.datetime.now().year - 1920)
+def years_word(n: int) -> str:
+    n = abs(n)  
+    if 11 <= n % 100 <= 14:
+        return "лет"
+    last = n % 10
+    if last == 1:
+        return "год"
+    if 2 <= last <= 4:
+        return "года"
+    return "лет"
 
 
+rendered_page = template.render(
+    age=age,
+    years_word=years_word(int(age)),
+    data=data,
+    
+)
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
